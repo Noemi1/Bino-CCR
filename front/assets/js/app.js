277a93bc1a355b37fef5165d69dab3bc796b7a28
Noemi1 <calmeida.no@gmail.com>
2020-06-14 20:09:26 -0300

    Ajustes gerais

--- front/assets/js/app.js
+++ front/assets/js/app.js
@@ -8,3 +8,16 @@ $('buttom.close').on('click', function() {
     $(this).parent('.notf').remove();
 })
 $('.celefone-mask').mask('(00) 0.0000-0000')
+
+$('.exercicios-slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    infinite: false,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 10000
+})
